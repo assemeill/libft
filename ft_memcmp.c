e849de007d8754563a596961dcbf8ab1a3f3fcdc
Assem Zhilkibayeva <aszhilki@e1z2r5p30.42.us.org>
2019-10-07 12:43:51 -0700

    update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,33 +6,22 @@
 /*   By: aszhilki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:53:15 by aszhilki          #+#    #+#             */
-/*   Updated: 2019/10/07 12:41:56 by aszhilki         ###   ########.fr       */
+/*   Updated: 2019/10/07 12:43:34 by aszhilki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t				l;
-	unsigned const char	*str1;
-	unsigned const char *str2;
-
-	l = 1;
-	str1 = s1;
-	str2 = s2;
 	if (n == 0)
 		return (0);
-	while (l <= n)
+	while (n--)
 	{
-		if (*str1++ == *str2++)
-		{
-			if (l == n)
-				return (0);
-		}
-		if (*str1 != *str2)
-			return (*str1 - *str2);
-		l++;
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		s1++;
+		s2++;
 	}
-	return (NULL);
+	return (0);
 }
