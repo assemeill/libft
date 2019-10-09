8cfe3e51bb7e5ba76c32fcf67c75adbfdbe0a839
Assem Zhilkibayeva <aszhilki@e1z2r5p33.42.us.org>
2019-10-08 22:16:59 -0700

    Updated according to Norm

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: aszhilki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 19:32:33 by aszhilki          #+#    #+#             */
-/*   Updated: 2019/10/08 21:40:33 by aszhilki         ###   ########.fr       */
+/*   Updated: 2019/10/08 22:07:20 by aszhilki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,32 +26,31 @@ static char		*ft_copystr(char *dest, char *src, int n)
 	return (dest);
 }
 
-char	**ft_strsplit(char const *s, char c)
+char			**ft_strsplit(char const *s, char c)
 {
-	int		n;
-	int		w;
+	int		n[2];
 	char	**str;
 
 	if (!s || !c
 	|| !(str = (char **)malloc(sizeof(char *) * (ft_words_count(s, c) + 1))))
 		return (NULL);
-	n = 0;
+	n[0] = 0;
 	while (*s)
 	{
 		while (*s == c)
 			s++;
 		if (*s != c && *s != '\0')
 		{
-			w = 0;
-			while (*(s + w) != c && *(s + w) != '\0')
-				w++;
-			if (!(str[n] = (char *)malloc(sizeof(char) * (w + 1))))
+			n[1] = 0;
+			while (*(s + n[1]) != c && *(s + n[1]) != '\0')
+				n[1]++;
+			if (!(str[n[0]] = (char *)malloc(sizeof(char) * (n[1] + 1))))
 				return (NULL);
-			str[n] = ft_copystr(str[n], (char *)s, w);
-			s += w;
-			++n;
+			str[n[0]] = ft_copystr(str[n[0]], (char *)s, n[1]);
+			s += n[1];
+			++n[0];
 		}
 	}
-	str[n] = NULL;
+	str[n[0]] = NULL;
 	return (str);
 }
