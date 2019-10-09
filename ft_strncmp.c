13f935646d8bfb052ecd90c13d8d3a2c43ca3e7b
Assem Zhilkibayeva <aszhilki@e1z2r5p30.42.us.org>
2019-10-09 11:03:33 -0700

    Updated for moulibft

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aszhilki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/13 10:33:52 by aszhilki          #+#    #+#             */
-/*   Updated: 2019/10/08 20:06:02 by aszhilki         ###   ########.fr       */
+/*   Updated: 2019/10/09 11:02:43 by aszhilki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n-- > 0)
+	while (n-- > 0 && (*s1 || *s2))
 	{
 		if (*s1 == *s2)
 		{
 			s1++;
 			s2++;
-			if (*s2 == '\0')
-				return (0);
 		}
 		else if (*s1 != *s2)
 			return ((unsigned char)(*s1) - (unsigned char)(*s2));
